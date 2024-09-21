5d57534c5d13c86d4c319fdca7abeb155932561e
ItsUriel18 <uriel12mtzhdz@gmail.com>
2024-09-21 12:15:12 -0600

    Add files via upload

---
+++ src/TipoToken.java
@@ -0,0 +1,13 @@
+public enum TipoToken {
+    IDENTIFICADOR,
+
+    // Palabras reservadas
+    AND, DISTINCT, FALSE, FROM, IS, NOT, NULL, OR, SELECT, TRUE, WHERE,
+
+    COMA, SEMICOLON, DOT, LEFT_PAREN, RIGHT_PAREN,
+    LT, LE, GT, GE, EQUAL, NE,
+    PLUS, MINUS, STAR, SLASH,
+
+    // Final de cadena
+    EOF
+}
